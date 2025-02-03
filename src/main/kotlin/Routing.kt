0931da8516f5996c456136fd2da6ea008be39ca9
Daniil Damarad <danilmega2003@gmail.com>
2025-02-03 21:29:24 +0300

    Initial commit

---
+++ src/main/kotlin/Routing.kt
@@ -0,0 +1,13 @@
+package ru.point
+
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+fun Application.configureRouting() {
+    routing {
+        get("/") {
+            call.respondText("This is AutoHub")
+        }
+    }
+}
